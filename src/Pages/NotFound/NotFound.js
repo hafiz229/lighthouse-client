92f9e1d5503a588748435477c0f98001dba6db0f
Hafiz <hasnathafiz229>
2021-12-18 23:30:08 +0600

    NotFound Responsive

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,16 +1,19 @@
 import React from "react";
 import Navigation from "../Shared/Navigation/Navigation";
+import "./NotFound.css";
 
 const NotFound = () => {
   return (
     <div>
       <Navigation></Navigation>
-      <div className="py-5">
+      <div className="py-5 notfound-img">
         <img
           src="https://s.udemycdn.com/error_page/error-desktop-v1.jpg"
           alt=""
         />
-        <h3 className="fw-bold">We can’t find the page you’re looking for</h3>
+        <h3 className="fw-bold pb-5">
+          We can’t find the page you’re looking for
+        </h3>
       </div>
     </div>
   );
